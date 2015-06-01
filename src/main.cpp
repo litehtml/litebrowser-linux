4ad65fc2b74f8acc9eabfdc92acc1a2c2f371f5e
Yuri Kobets <yuri.kobets@gmail.com>
2015-06-02 01:05:43 +0300

    Updated to the latest litehtml Added CMakeLists.txt to build with cmake Updated the way we embedd the master.css

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,12 @@
 #include "globals.h"
 #include "browser_wnd.h"
 
+char master_css[] = 
+{
+#include "master.css.inc"
+,0
+};
+
 extern "C" char _binary_litehtml_include_master_css_start;
 extern "C" char _binary_litehtml_include_master_css_end;
 
@@ -14,11 +20,11 @@ int main (int argc, char *argv[])
 {
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "litehtml.browser");
 
-	litehtml::tstring css;
-	css.append(&_binary_litehtml_include_master_css_start, &_binary_litehtml_include_master_css_end - &_binary_litehtml_include_master_css_start);
+	//litehtml::tstring css;
+//	css.append(&_binary_litehtml_include_master_css_start, &_binary_litehtml_include_master_css_end - &_binary_litehtml_include_master_css_start);
 
 	litehtml::context html_context;
-	html_context.load_master_stylesheet(css.c_str());
+	html_context.load_master_stylesheet(master_css);
 
 	browser_window win(&html_context);
 
